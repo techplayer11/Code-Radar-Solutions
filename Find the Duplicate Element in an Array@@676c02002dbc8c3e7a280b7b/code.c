b0d93acf7c528b2bae141e64072dcf8dfca624a5
Ridhi Sharma <theridhiisharmaa@gmail.com>
2025-03-28 16:23:38 +0530

    Solution to Find the Duplicate Element in an Array

--- Find the Duplicate Element in an Array@@676c02002dbc8c3e7a280b7b/code.c
+++ Find the Duplicate Element in an Array@@676c02002dbc8c3e7a280b7b/code.c
@@ -7,7 +7,7 @@ int main(){
         scanf("%d",&arr[i]);
     }
     for(int i=0;i<n;i++){
-        for(int j=i+1;i<n;j++){
+        for(int j=i+1;j<n;j++){
             if(arr[i]==arr[j]){
                 printf("%d",arr[i]);
             }
